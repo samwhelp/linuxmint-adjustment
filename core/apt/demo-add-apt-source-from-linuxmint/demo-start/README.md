2a47eef699368c2c9b3dbe7b006e9ea608c58574
samwhelp <samwhelp@users.noreply.github.com>
2026-07-12 10:37:04 +0800

    fix

---
+++ core/apt/demo-add-apt-source-from-linuxmint/demo-start/README.md
@@ -0,0 +1,41 @@
+
+
+# demo-add-apt-source-from-linuxmint
+
+
+
+
+## Subject
+
+* [Explore](#explore)
+* [Link](#link)
+
+
+
+
+## Explore
+
+run
+
+``` sh
+apt-cache search mint-meta
+```
+
+show
+
+```
+mint-meta-cinnamon - Set of packages installed by default in the Cinnamon edition of Linux Mint
+mint-meta-codecs - All the packages required for multimedia support in Linux Mint.
+mint-meta-core - Core Linux Mint packages
+mint-meta-mate - Set of packages installed by default in the MATE edition of Linux Mint
+mint-meta-xfce - Set of packages installed by default in the Xfce edition of Linux Mint
+```
+
+
+
+
+## Link
+
+* [https://github.com/clefebvre/docker-images](https://github.com/clefebvre/docker-images)
+* [https://github.com/clefebvre/docker-images/blob/master/mint23-amd64.Dockerfile](https://github.com/clefebvre/docker-images/blob/master/mint23-amd64.Dockerfile)
+* [https://github.com/clefebvre/docker-images/tree/master/mint23/etc/apt](https://github.com/clefebvre/docker-images/tree/master/mint23/etc/apt)
---
+++ core/apt/demo-add-apt-source-from-linuxmint/demo-start/add-apt-source-from-linuxmint.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+
+
+
+##
+## ## Module
+##
+
+function add_linuxmint_keyring () {
+
+	#wget http://packages.linuxmint.com/pool/main/l/linuxmint-keyring/linuxmint-keyring_2022.06.21_all.deb -O /tmp/linuxmint-keyring_2022.06.21_all.deb > /dev/null 2>&1
+
+	wget -c http://packages.linuxmint.com/pool/main/l/linuxmint-keyring/linuxmint-keyring_2022.06.21_all.deb -O /tmp/linuxmint-keyring_2022.06.21_all.deb
+
+	echo sudo dpkg -i /tmp/linuxmint-keyring_2022.06.21_all.deb
+
+	sudo dpkg -i /tmp/linuxmint-keyring_2022.06.21_all.deb
+
+	rm -f /tmp/linuxmint-keyring_2022.06.21_all.deb
+
+}
+
+function add_linuxmint_apt_sources () {
+
+	sudo mkdir -p "/etc/apt/sources.list.d"
+	cat << __EOF__ | sudo tee "/etc/apt/sources.list.d/linuxmint.sources" > /dev/null 2>&1
+Types: deb
+URIs: http://packages.linuxmint.com/
+Suites: alfa
+Components: main upstream import backport
+Architectures: amd64
+Signed-By: /etc/apt/trusted.gpg.d/linuxmint-keyring.gpg
+__EOF__
+
+}
+
+function add_linuxmint_apt_preferences () {
+
+cat << __EOF__ | sudo tee "/etc/apt/preferences.d/linuxmint.pref"  > /dev/null 2>&1
+Package: *
+Pin: origin live.linuxmint.com
+Pin-Priority: 750
+
+Package: *
+Pin: release o=linuxmint,c=upstream
+Pin-Priority: 700
+__EOF__
+
+}
+
+
+
+
+##
+## ## Main
+##
+
+add_linuxmint_keyring
+
+add_linuxmint_apt_sources
+
+add_linuxmint_apt_preferences
